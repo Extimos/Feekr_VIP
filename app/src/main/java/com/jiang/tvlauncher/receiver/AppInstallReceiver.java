7f78b5d42c785c34328a26ed0813f31f6b16c6af
jiangadmin <www.fangmu@qq.com>
2018-09-12 22:49:13 +0800

    安装后直接调起

---
+++ app/src/main/java/com/jiang/tvlauncher/receiver/AppInstallReceiver.java
@@ -0,0 +1,50 @@
+package com.jiang.tvlauncher.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.jiang.tvlauncher.dialog.Loading;
+import com.jiang.tvlauncher.entity.Const;
+import com.jiang.tvlauncher.servlet.GetVIP_Servlet;
+import com.jiang.tvlauncher.utils.LogUtil;
+import com.jiang.tvlauncher.utils.SaveUtils;
+
+/**
+ * @author: jiangadmin
+ * @date: 2017/7/3.
+ * @Email: www.fangmu@qq.com
+ * @Phone: 186 6120 1018
+ * TODO: 应用安装/卸载广播监听
+ */
+
+public class AppInstallReceiver extends BroadcastReceiver {
+    private static final String TAG = "AppInstallReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //安装
+        if (intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
+            String packageName = intent.getData().getSchemeSpecificPart();
+
+            LogUtil.e(TAG, "安装成功");
+            Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+            if (launchIntent != null) {
+                context.startActivity(launchIntent);
+            }
+        }
+        //卸载
+        if (intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)) {
+            String packageName = intent.getData().getSchemeSpecificPart();
+
+
+        }
+        //替换
+        if (intent.getAction().equals(Intent.ACTION_PACKAGE_REPLACED)) {
+
+            LogUtil.e(TAG, "替换成功");
+        }
+    }
+}
